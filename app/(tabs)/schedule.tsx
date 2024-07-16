c82022f6db7d88273d2efe0ca022e5479e1051e2
geon-2 <gunle0416@gmail.com>
2024-07-16 20:36:47 +0900

    Feat: Add bottomTabNavigator to main screen for routing

--- app/(tabs)/schedule.tsx
+++ app/(tabs)/schedule.tsx
@@ -1,6 +1,6 @@
 import { Text, View } from "react-native";
 
-export default function HomeScreen() {
+export default function ScheduleScreen() {
   return (
     <View
       style={{
@@ -9,7 +9,7 @@ export default function HomeScreen() {
         alignItems: "center",
       }}
     >
-      <Text>Home Page</Text>
+      <Text>Schedule Page</Text>
     </View>
   );
 }
