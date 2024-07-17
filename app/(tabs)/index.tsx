5f4c07ad0f4762bc02724d07182b3caa8d26da18
geon-2 <gunle0416@gmail.com>
2024-07-17 15:31:07 +0900

    Feat: Install webview and test 웹뷰를 설치하고, 호스팅되고 있는 웹서비스를 통해 라이브러리 테스트를 진행하였고, 정상적으로 동작하는 것을 확인하였습니다.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,17 @@
-import { Text, View } from "react-native";
+import { View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function HomeScreen() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
       }}
     >
-      <Text>Home Page</Text>
+      <WebView
+        source={{ uri: 'https://extra-react-webview.vercel.app/' }}
+        style={{ flex: 1 }}
+      />
     </View>
   );
 }
--- app/(tabs)/schedule.tsx
+++ app/(tabs)/schedule.tsx
@@ -1,12 +1,12 @@
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native';
 
 export default function ScheduleScreen() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
       <Text>Schedule Page</Text>
