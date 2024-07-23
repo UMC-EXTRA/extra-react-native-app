c0f24a38ed575eff80c7268b86494c2e07622f14
geon-2 <gunle0416@gmail.com>
2024-07-24 01:01:32 +0900

    Feat: Add request permission 위치, 카메라, 푸시 알림 권한 요청에 대한 기능 추가 - expo-notifications, expo-location, expo-camera 사용 - 권한 요청 및 권한 상태 확인 기능 테스트 - 알림 혀용 토글 버튼 테스트 - 카메라 기능 일부 테스트
    추후 개발 예정
    - 회원가입 프로세스에 권한 요청 프로세스 추가
    - 카메라 페이지 디자인 및 기능 테스트
    - QR 스캐너 테스트

--- app/index.tsx
+++ app/index.tsx
@@ -26,7 +26,7 @@ const Index = () => {
     return null;
   }
 
-  return <Redirect href="/member" />;
+  return <Redirect href="/permission" />;
 };
 
 export default Index;
---
+++ scripts/permission.ts
@@ -0,0 +1,104 @@
+import { Alert, Linking, Platform } from 'react-native';
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+import * as Location from 'expo-location';
+import { Camera } from 'expo-camera';
+import * as IntentLauncher from 'expo-intent-launcher';
+
+export async function requestPushNotificationPermission(): Promise<boolean> {
+  const { status: existingStatus } = await Notifications.getPermissionsAsync();
+  let finalStatus = existingStatus;
+  if (existingStatus !== 'granted') {
+    const { status } = await Notifications.requestPermissionsAsync();
+    finalStatus = status;
+  }
+  if (finalStatus !== 'granted') {
+    return false;
+  }
+  return true;
+}
+
+export async function requestLocationPermission(): Promise<boolean> {
+  const { status } = await Location.requestForegroundPermissionsAsync();
+  if (status !== 'granted') {
+    Alert.alert(
+      '권한 필요',
+      '이 앱을 사용하기 위해서는 위치 권한이 필요합니다. 설정에서 권한을 활성화해주세요.',
+      [
+        { text: '취소', style: 'cancel' },
+        { text: '설정으로 이동', onPress: openSettings },
+      ],
+    );
+    return false;
+  }
+  return true;
+}
+
+export async function requestCameraPermission(): Promise<boolean> {
+  const { status } = await Camera.requestCameraPermissionsAsync();
+  if (status !== 'granted') {
+    Alert.alert(
+      '권한 필요',
+      '해당 기능은 카메라 권한이 필요합니다. 설정에서 권한을 활성화해주세요.',
+      [
+        { text: '취소', style: 'cancel' },
+        { text: '설정으로 이동', onPress: openSettings },
+      ],
+    );
+    return false;
+  }
+  return true;
+}
+
+export async function togglePushNotificationPermission(
+  isEnabled: boolean,
+): Promise<boolean> {
+  const { status } = await Notifications.getPermissionsAsync();
+
+  if (isEnabled) {
+    if (status != 'granted') {
+      return true;
+    }
+    Alert.alert(
+      '알림 비활성화',
+      '알림을 비활성화하려면 설정에서 수동으로 변경해야 합니다. 설정으로 이동하시겠습니까?',
+      [
+        { text: '취소', style: 'cancel' },
+        { text: '설정으로 이동', onPress: openSettings },
+      ],
+    );
+    return false;
+  } else {
+    if (status == 'granted') {
+      return true;
+    }
+    Alert.alert(
+      '알림 활성화',
+      '알림을 활성화하려면 설정에서 수동으로 변경해야 합니다. 설정으로 이동하시겠습니까?',
+      [
+        { text: '취소', style: 'cancel' },
+        { text: '설정으로 이동', onPress: openSettings },
+      ],
+    );
+    return false;
+  }
+}
+
+function openSettings() {
+  if (Platform.OS === 'ios') {
+    Linking.openURL('app-settings:');
+  } else {
+    const packageName =
+      Constants.manifest?.android?.package ||
+      Constants.manifest2?.android?.package;
+    if (packageName) {
+      IntentLauncher.startActivityAsync(
+        IntentLauncher.ActivityAction.APPLICATION_DETAILS_SETTINGS,
+        { data: 'package:' + packageName },
+      );
+    } else {
+      console.error('Cannot find package name for Android');
+      // 패키지 이름을 찾을 수 없는 경우에 대한 대체 처리
+    }
+  }
+}
