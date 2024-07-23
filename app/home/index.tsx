578931da5c34c19640f2bde839cd03d144ea7b53
geon-2 <gunle0416@gmail.com>
2024-07-23 21:13:16 +0900

    Feat: Add header in home screen - 홈화면에 헤더 영역 UI를 추가함

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,5 +1,13 @@
-import { View, StyleSheet } from 'react-native';
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Text,
+} from 'react-native';
+import { Image } from 'expo-image';
 import { WebView } from 'react-native-webview';
+import { AntDesign } from '@expo/vector-icons';
 import getSize from '@/scripts/getSize';
 
 const HomeScreen = () => {
@@ -9,20 +17,105 @@ const HomeScreen = () => {
         flex: 1,
       }}
     >
-      <View
-        style={{ width: '100%', height: getSize(261), backgroundColor: '#000' }}
-      ></View>
+      <View style={styles.header}>
+        <View style={styles.menuContainer}>
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.input}
+              placeholder="찾으시는 공고 있으세요?"
+              placeholderTextColor="#DDD"
+            />
+            <TouchableOpacity>
+              <AntDesign name="search1" size={getSize(21)} color="white" />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.iconContainer}>
+            <TouchableOpacity>
+              <Image
+                style={styles.image}
+                source={require('@/assets/images/icons/QR.png')}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Image
+                style={styles.image}
+                source={require('@/assets/images/icons/Ball.png')}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={styles.bannerAds}>
+          <Text style={{ color: '#fff', fontSize: 20, fontWeight: '800' }}>
+            광고
+          </Text>
+        </View>
+      </View>
       <WebView
         // 아래 링크를 수정하세요.
         source={{ uri: 'https://extra-react-webview.vercel.app/' }}
-        style={{ flex: 1 }}
+        style={styles.webView}
       />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  webViewContainer: {
+  header: {
+    width: '100%',
+    height: getSize(261),
+    backgroundColor: '#000',
+  },
+  menuContainer: {
+    marginTop: getSize(112),
+    width: '100%',
+    height: getSize(37),
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  inputContainer: {
+    width: getSize(262),
+    height: '100%',
+    borderRadius: getSize(30),
+    borderStyle: 'solid',
+    borderWidth: 2,
+    borderColor: '#ADADAD',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: getSize(16),
+    marginLeft: getSize(12),
+  },
+  input: {
+    color: '#fff',
+    fontSize: getSize(11),
+    fontFamily: 'Inter',
+    fontWeight: '400',
+  },
+  iconContainer: {
+    flexDirection: 'row',
+    marginRight: getSize(34),
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '100%',
+    width: getSize(80),
+  },
+  image: {
+    width: getSize(34),
+    height: getSize(34),
+  },
+  bannerAds: {
+    width: getSize(412),
+    height: getSize(86),
+    marginTop: getSize(26),
+    marginHorizontal: 'auto',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderStyle: 'solid',
+    borderColor: '#9a9a9a',
+    borderWidth: 3,
+  },
+  webView: {
     flex: 1,
   },
 });
